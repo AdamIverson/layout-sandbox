ca665a63abec90f59a70c27910f0bae4538e46a8
Adam Iverson <aciverso@gmail.com>
2023-04-30 18:01:39 -0500

    stylesheet connected to Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "@/pages/Header";
 import Footer from "@/pages/Folder";
+import styles from "./layout.module.css"
   
 const Layout = ({ children }) => {
   return (
     <>
+    <div className={styles.container}>
       <Header />
       {children}
       <Footer />
+    </div>
     </>
   );
 };
