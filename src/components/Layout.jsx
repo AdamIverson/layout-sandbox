46ac5423b24e3c9675d1cf56da493d8f702d3b97
Adam Iverson <aciverso@gmail.com>
2023-05-02 16:06:59 -0500

    footer not folder

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "@/pages/Header";
-import Footer from "@/pages/Folder";
+import Footer from "@/pages/Footer";
 import styles from "./layout.module.css"
   
 const Layout = ({ children }) => {
