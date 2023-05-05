44b02f1039ceb11403169f1433a1bc5a880ebc09
Adam Iverson <aciverso@gmail.com>
2023-05-05 09:54:12 -0500

    ok now i understand styles

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import style from './Header.module.css'
+import styles from './Header.module.css'
 
 const Header = () => {
-  return <h3 className="test">adam iverson</h3>;
+  return <h3 className={styles.test}>adam iverson</h3>;
 };
 
 export default Header;
