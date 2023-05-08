3cb74621db3220215046a0eceebd6dcb49017c1f
Adam Iverson <aciverso@gmail.com>
2023-05-08 08:42:18 -0500

    gtag

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,6 +6,7 @@ const About = () => {
   return(
     <div className={styles.pagetitle}>
       <h1>aboot</h1>
+      <Card topic='Aboot' />
       <p>ten feet tall and bulletproof</p>
     </div>
   )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,21 @@
 import '@/styles/globals.css'
 import Layout from '@/components/Layout'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-J0KNJYGTLH"></Script>
+      <Script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'G-J0KNJYGTLH');
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
