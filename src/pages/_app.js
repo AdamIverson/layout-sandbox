ed5abefce2dabf4ab1e9c59cedb2ebe3ba53a6d5
Adam Iverson <aciverso@gmail.com>
2023-05-08 08:54:57 -0500

    analytics tweaks

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-J0KNJYGTLH"/>
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-J0KNJYGTLH"></Script>
       <Script>
         window.dataLayer = window.dataLayer || [];
