189dc4d03f47cab4f0f546fc75b2cc2355fc8c17
Adam Iverson <aciverso@gmail.com>
2023-05-08 11:00:21 -0500

    author list

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -5,11 +5,19 @@ const Author = () => {
   return (
     <>
       <h1>Author page</h1>
-      <h2>Don't Fall In Love With The City</h2>
-      <p>collaboration of joe bozic's poetry and adam iverson's photography</p>
-      <h2>gelato for dinner</h2>
-      <p>shameless street photography, italy 2019</p>
-      <Link href="https://www.mcsweeneys.net/articles/things-my-girlfriend-did-not-say-after-i-told-her-that-i-sometimes-feel-stuck">dave eggers and i are besties</Link>
+      <ul>
+        <li>
+          <h2>Don't Fall In Love With The City</h2>
+          <p>collaboration of joe bozic's poetry and adam iverson's photography</p>
+        </li>
+        <li>
+          <h2>gelato for dinner</h2>
+          <p>shameless street photography, italy 2019</p>
+        </li>
+        <li>
+          <Link href="https://www.mcsweeneys.net/articles/things-my-girlfriend-did-not-say-after-i-told-her-that-i-sometimes-feel-stuck">dave eggers and i are besties</Link>
+        </li>
+      </ul>
     </>
   )
 }
