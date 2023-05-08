236acf304bf251874d3c61284e4ddfc3b8f3b6c9
Adam Iverson <aciverso@gmail.com>
2023-05-08 10:39:53 -0500

    d.eggs

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -9,7 +9,7 @@ const Author = () => {
       <p>collaboration with joe bozic, poet, finding beauty in the corners in tandem with my photography</p>
       <h2>gelato for dinner</h2>
       <p>shameless street photography, italy 2019</p>
-      <Link href="https://www.mcsweeneys.net/articles/things-my-girlfriend-did-not-say-after-i-told-her-that-i-sometimes-feel-stuck">dramedy</Link>
+      <Link href="https://www.mcsweeneys.net/articles/things-my-girlfriend-did-not-say-after-i-told-her-that-i-sometimes-feel-stuck">dave eggers and i are besties</Link>
     </>
   )
 }
