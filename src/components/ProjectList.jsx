16af4389cc034353daecca9cbae1cd428ee5d2d3
Adam Iverson <aciverso@gmail.com>
2023-05-09 08:55:03 -0500

    links to repos in project list

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 const ProjectList = () => {
   return (
     <ul>
-      <li>Solcana Fitness</li>
-      <li>Pet-Jet</li>
-      <li>hatclub</li>
+      <li>
+        <h1>Solcana Fitness</h1>
+        <Link href="https://github.com/AdamIverson/solcanaThree">Link to Repo</Link>
+      </li>
+      <li>
+        <p>Pet-Jet</p>
+        <Link href="https://github.com/petjetprivate/pet-jet">Link to Repo</Link>
+      </li>
+      <li>
+        <p>hatclub</p>
+        <Link href="https://github.com/AdamIverson/Hatclub-The-Premier-Hat-Appreciation-App">Link to Repo</Link>
+      </li>
     </ul>
   )
 }
