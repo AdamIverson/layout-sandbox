5ff9ea5802a8543d0321628ba808d23cc8ef7613
Adam Iverson <aciverso@gmail.com>
2023-05-09 12:25:49 -0500

    adamiverson dot com into data

--- src/data/projectData.jsx
+++ src/data/projectData.jsx
@@ -1,5 +1,10 @@
 const projectData = [
   {
+    title: 'adamiverson.com',
+    tech: ['Next.js', 'React', 'urmom'],
+    url: 'https://github.com/AdamIverson/layout-sandbox'
+  },
+  {
     title: 'Solcana Fitness',
     tech: ['Expo', 'React Native', 'Firebase'],
     url: 'https://github.com/AdamIverson/solcanaThree'
