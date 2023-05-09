3b417ce6be510d11c368c7aa3792f3db282aae40
Adam Iverson <aciverso@gmail.com>
2023-05-09 12:17:52 -0500

    project data

---
+++ src/data/project-data.jsx
@@ -0,0 +1,17 @@
+const projectData = [
+  {
+    title: 'Solcana Fitness',
+    tech: ['Expo', 'React Native', 'Firebase'],
+    url: 'https://github.com/AdamIverson/solcanaThree'
+  },
+  {
+    title: 'Pet-Jet',
+    tech: ['React'],
+    url: 'https://github.com/petjetprivate/pet-jet'
+  },
+  {
+    title: 'hatclub',
+    tech: 'React',
+    url: 'https://github.com/AdamIverson/Hatclub-The-Premier-Hat-Appreciation-App'
+  }
+]
