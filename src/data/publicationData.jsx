a0e75695675a1d8824c757e090ab9390d2eb534a
Adam Iverson <aciverso@gmail.com>
2023-05-10 09:18:50 -0500

    cleanup to mollify netlify

--- src/data/publicationData.jsx
+++ src/data/publicationData.jsx
@@ -1,21 +1,21 @@
 const publicationData = [
   {
-    title: "Don't Fall in Love with the City",
-    detail: "collaboration of joe bozic's poetry and adam iverson's photography",
-    image: "/../public/assets/northern.spark.jpg",
-    url: "https://www.adamiversonphotography.com"
+    title: 'Dont Fall in Love with the City',
+    detail: 'collaboration of joe bozics poetry and adam iversons photography',
+    image: '/../public/assets/northern.spark.jpg',
+    url: 'https://www.adamiversonphotography.com'
   },
   {
-    title: "gelato for dinner",
-    detail: "street photography, italy, summer 2019",
-    image: "/../public/assets/italy.jpg",
-    url: "https://www.adamiversonphotography.com"
+    title: 'gelato for dinner',
+    detail: 'street photography, italy, summer 2019',
+    image: '/../public/assets/italy.jpg',
+    url: 'https://www.adamiversonphotography.com'
   },
   {
-    title: "dave eggers and i are best friends",
-    detail: "she married me anyway",
-    url: "https://www.mcsweeneys.net/articles/things-my-girlfriend-did-not-say-after-i-told-her-that-i-sometimes-feel-stuck",
-    image: "/../public/assets/mcsweeneys.png"
+    title: 'dave eggers and i are best friends',
+    detail: 'she married me anyway',
+    url: 'https://www.mcsweeneys.net/articles/things-my-girlfriend-did-not-say-after-i-told-her-that-i-sometimes-feel-stuck',
+    image: '/../public/assets/mcsweeneys.png'
   }
 ]
 
