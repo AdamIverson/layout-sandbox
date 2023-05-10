528bf193ce60c6038d49c6828eac33a033036aa1
Adam Iverson <aciverso@gmail.com>
2023-05-10 18:49:33 -0500

    placeholder

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from 'next/image';
 
 export default function Card({ project }) {
+  console.log('project:', project);
   // const [projectState, setProjectState] = useState();
 
   // useEffect(() => {
@@ -12,10 +13,11 @@ export default function Card({ project }) {
   return (
     <li>
       <ul>
-        <li>{project.title}</li>
+        <li>placeholder list item</li>
+        {/* <li>{project.title}</li>
         <li>{project.tech}</li>
         <Link href={project.url}>Link to Repo</Link>
-        <Image src={project.image} height={1200} width={800} alt="image"></Image>
+        <Image src={project.image} height={1200} width={800} alt="image"></Image> */}
       </ul >
     </li>
   )
