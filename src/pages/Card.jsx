a01914805dcc507861d32c324796027e4091b888
Adam Iverson <aciverso@gmail.com>
2023-05-11 13:07:13 -0500

    test paragraph list to check netlify errors

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from 'next/image';
 
 export default function Card({ project }) {
-//   console.log("+*+*+*+*+*+*project:", project);
+  console.log("+*+*+*+*+*+*project:", project);
 // const [test, setTest] = useState();
 
 // useEffect(() => {
@@ -13,16 +13,17 @@ export default function Card({ project }) {
 
   return (
     <li>
-      <ul>
+      <p>test paragraph</p>
+      {/* <ul> */}
       {/* <li>{test.title}</li>
         <li>{test.tech}</li>
         <Link href={test.url}>Link to Repo</Link>
         <Image src={test.image} height={1200} width={800} alt="image"></Image> */}
-        <li>{project.title}</li>
+        {/* <li>{project.title}</li>
         <li>{project.tech}</li>
-        <Link href={project.url}>Link to Repo</Link>
+        <Link href={project.url}>Link to Repo</Link> */}
         {/* <Image src={project.image} height={1200} width={800} alt="image"></Image> */}
-      </ul >
+      {/* </ul > */}
     </li>
   )
 }
