3d7672cfdf2f9cbd99566c0399462cecbed051d1
Adam Iverson <aciverso@gmail.com>
2023-05-11 15:52:02 -0500

    conditional in Card solved prerendering issue locally, now trying netlify

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -4,27 +4,35 @@ import Image from 'next/image';
 
 export default function Card({ project }) {
   console.log("+*+*+*+*+*+*project:", project);
-const [test, setTest] = useState();
+  const [test, setTest] = useState();
 
-useEffect(() => {
-  setTest(project);
-}, []);
+  useEffect(() => {
+    setTest(project);
+  }, [test]);
 
+  console.log("+*+*+*+*test", test);
 
-  return (
-    <li>
-      <p>test paragraph</p>
-      <p>{test.title}</p>
-      {/* <ul> */}
-      {/* <li>{test.title}</li>
+  if (test) {
+    return (
+      <li>
+        {/* <p>test paragraph</p>
+        <p>{test.title}</p> */}
+        <ul>
+        <li>{test.title}</li>
         <li>{test.tech}</li>
         <Link href={test.url}>Link to Repo</Link>
-        <Image src={test.image} height={1200} width={800} alt="image"></Image> */}
+      {/* <Image src={test.image} height={1200} width={800} alt="image"></Image> */}
         {/* <li>{project.title}</li>
         <li>{project.tech}</li>
-        <Link href={project.url}>Link to Repo</Link> */}
+      <Link href={project.url}>Link to Repo</Link> */}
         {/* <Image src={project.image} height={1200} width={800} alt="image"></Image> */}
-      {/* </ul > */}
-    </li>
-  )
+        </ul >
+      </li>
+    )
+  }
+  else {
+    return (
+      <li>placeholder</li>
+    )
+  }
 }
