f2bf2feafa27bd4310cf4515d4fa73ca5c5d349f
Adam Iverson <aciverso@gmail.com>
2023-05-11 10:00:56 -0500

    file organizing

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Header from "@/pages/Header";
-import Navbar from "@/pages/Navbar";
-import Footer from "@/pages/Footer";
-import styles from "./layout.module.css"
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import styles from ".//../styles/layout.module.css"
   
 const Layout = ({ children }) => {
   return (
