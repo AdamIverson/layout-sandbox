4b724dbc2dc6f53d0e2ff241688580e68b49a387
Adam Iverson <aciverso@gmail.com>
2023-05-11 11:38:08 -0500

    containrd to container in Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,7 @@ import styles from ".//../styles/layout.module.css"
 const Layout = ({ children }) => {
   return (
     <>
-    <div className={styles.containerd}>
+    <div className={styles.container}>
       <Header />
       <Navbar />
       {children}
